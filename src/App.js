e377f5b625f1f28cf7d8cc425df3b8a1a4da5e50
dustwise <jared.mohney@gmail.com>
2017-07-30 06:48:23 -0600

    h1 baby

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <a href="http://protopage.dustwise.com">Protopage</a>
-        <a href="http://pogtracker.com">Pogtracker</a>
+        <h1><a href="http://protopage.dustwise.com">Protopage</a></h1>
+        <h1><a href="http://pogtracker.com">Pogtracker</a></h1>
       </div>
     );
   }
