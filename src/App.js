d0846c4929aba0a9fc4f5194f623e75e6a28e202
dustwise <jared.mohney@gmail.com>
2017-08-16 04:58:02 -0600

    parsing embedded template string functional

--- src/App.js
+++ src/App.js
@@ -29,7 +29,27 @@ class App extends Component {
 
         <CodeMirrorW>
           <CodeMirror
-            value='function(num){num * 2}'
+            value={
+`class JaredMohney extends Developer {
+  constructor(desireToWork, projects){
+    super(desireToWork, projects);
+
+    //important information I'm pretty sure
+    this.name = "Jared Mohney";
+    this.title = "Web Developer";
+    this.location = "Longmont, CO";
+    this.skills = ['Javascript', 'React', 'Redux', 'CSS3', 'HTML5', 'Express', 'Node'];
+    this.description = \`\${this.name}\ is a full stack javascript developer with an interest in esports, chinchillas and bettering himself through challenging work.\`;
+    this.github = "${<a href='http://www.github.com/dustwise'>test</a>}";
+    this.linkedIn = "http://www.linkedin.com/jaredmohney";
+    this.email = "jared.mohney@gmail.com"
+
+    //irrelevant?
+    // this.favoriteMovies = ["Little Shop of Horrors", "Aliens", "There Will Be Blood", "Terminator 2: Judgment Day"];
+    // this.favoriteGames = ["Shadow of the Colossus", "Kingdom Hearts 2", "Sonic The Hedgehog 3 & Knuckles", "DOTA2"];
+  }
+}`
+            }
             options={{
               mode: 'javascript',
               theme: 'dracula',
