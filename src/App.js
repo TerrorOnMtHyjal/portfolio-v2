a7806a1000d46e972b80b126c7f6a94929ff1c99
dustwise <jared.mohney@gmail.com>
2017-08-16 08:20:02 -0600

    transitions changed to css animations successful

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import CodeMirror from 'react-codemirror2';
 import Editor from './components/Editor';
 import Transition from 'react-transition-group/Transition';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 import './App.css';
 
@@ -12,20 +13,31 @@ const CodeMirrorW = styled.div`
   height: 100vh;
 `;
 
+const Glitch = ({ children, ...props }) => (
+  <CSSTransition
+    {...props}
+    timeout={2000}
+    classNames="glitch"
+    appear={true}
+  >
+    {children}
+  </CSSTransition>
+);
+
 const duration = 2000;
 
 const defaultStyle = {
   transition: `filter ${duration}ms ease-in-out`,
-  filter: 'grayscale(100%)',
+  filter: 'invert(100%)',
 }
 
 const transitionStyles = {
-  entering: { filter: 'grayscale(0)' },
-  entered:  { filter: 'grayscale(0)' },
+  entering: { filter: 'invert(0)' },
+  entered:  { filter: 'invert(0)' },
 };
 
 const Fade = ({ in: inProp }) => (
-  <Transition in={inProp} timeout={duration} appear="true">
+  <Transition in={inProp} timeout={duration} appear={true}>
     {(state) => (
       <div style={{
         ...defaultStyle,
@@ -52,13 +64,15 @@ class App extends Component {
     this.state = {
       in: true
     }
+
   }
 
   render() {
     return (
       <AppW className="App">
-        <Fade in={this.state.in}>
-        </Fade>
+        <Glitch in={this.state.in}>
+          <p>hello</p>
+        </Glitch>
       </AppW>
     );
   }
