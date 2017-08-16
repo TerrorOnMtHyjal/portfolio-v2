d5558583efdb876380fb4706fd00956c4f5f11ab
dustwise <jared.mohney@gmail.com>
2017-08-16 07:34:27 -0600

    basic animation functional

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import CodeMirror from 'react-codemirror2';
 import Editor from './components/Editor';
+import Transition from 'react-transition-group/Transition';
+
 import './App.css';
 
 const CodeMirrorW = styled.div`
@@ -10,17 +12,53 @@ const CodeMirrorW = styled.div`
   height: 100vh;
 `;
 
+const duration = 2000;
+
+const defaultStyle = {
+  transition: `filter ${duration}ms ease-in-out`,
+  filter: 'grayscale(100%)',
+}
+
+const transitionStyles = {
+  entering: { filter: 'grayscale(0)' },
+  entered:  { filter: 'grayscale(0)' },
+};
+
+const Fade = ({ in: inProp }) => (
+  <Transition in={inProp} timeout={duration} appear="true">
+    {(state) => (
+      <div style={{
+        ...defaultStyle,
+        ...transitionStyles[state]
+      }}>
+        <CodeMirrorW>
+          <Editor />
+        </CodeMirrorW>
+      </div>
+    )}
+  </Transition>
+);
+
+
 const AppW = styled.div`
 
 `;
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      in: true
+    }
+  }
+
   render() {
     return (
       <AppW className="App">
-        <CodeMirrorW>
-          <Editor />
-        </CodeMirrorW>
+        <Fade in={this.state.in}>
+        </Fade>
       </AppW>
     );
   }
