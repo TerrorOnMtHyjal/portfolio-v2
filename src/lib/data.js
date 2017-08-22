7a39bb3cf073fb70f60c9fcce6ff3b6b1116874c
dustwise <jared.mohney@gmail.com>
2017-08-22 10:56:08 -0600

    further info card colors

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,18 +1,18 @@
 export const code = {
   info: 
 `   constructor(desireToWork, projects){
-      super(desireToWork, projects);
+    super(desireToWork, projects);
 
-      this.name = 'Jared Mohney';
-      this.title = 'Web Developer';
-      this.location = 'Longmont, CO';
-      this.skills = [ 'Javascript', 'React', 'Redux', 'CSS3', 'HTML5', 'Express', 'Node', ];
-      this.description = \`\${this.name} is a full stack javascript developer with an interest in esports, chinchillas and bettering himself through challenging work.\`;
-      this.github = 'http://www.github.com/dustwise';
-      this.linkedIn = 'www.linkedin.com/in/jaredmohney';
-      this.email = 'jared.mohney@gmail.com';
+    this.name = 'Jared Mohney';
+    this.title = 'Web Developer';
+    this.location = 'Longmont, CO';
+    this.skills = [ 'Javascript', 'React', 'Redux', 'CSS3', 'HTML5', 'Express', 'Node', ];
+    this.description = \`\${this.name} is a full stack javascript developer with an interest in esports, chinchillas and bettering himself through challenging work.\`;
+    this.github = 'http://www.github.com/dustwise';
+    this.linkedIn = 'www.linkedin.com/in/jaredmohney';
+    this.email = 'jared.mohney@gmail.com';
 
-      }`,
+    }`,
 };
 
 export const comments = `
